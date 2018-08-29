103875c10194d40caf2a996ad352d794f6c1a1fc
Luis Reyes <lhreyes5@misena.edu.co>
2018-08-28 21:42:08 -0500

    henry 1

--- app/src/main/java/com/connorng/tequendama01/SplashActivity.java
+++ app/src/main/java/com/connorng/tequendama01/SplashActivity.java
@@ -1,5 +1,7 @@
 package com.connorng.tequendama01;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,13 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(),RegistroActivity.class);
+                startActivity(intent);
+            }
+        },3000);
     }
 }
